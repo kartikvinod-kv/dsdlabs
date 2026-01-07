a3bd2c4cdc27b3947c6772f39fcbfcb4e7f93bbd
kartikvinod-kv <kartikvinod.kv@gmail.com>
2026-01-07 09:19:11 +0530

    Update counter logic to handle decrementation
    Modified counter behavior to include decrementation based on sw0 input.

--- 03-pins/counter.sv
+++ 03-pins/counter.sv
@@ -1,14 +1,19 @@
 module counter (
     input logic clkpulse,       // Clock input
     input logic rst,         // Active low reset
+    input  logic       sw0,       // NEW: Mapped to Slide Switch 0
     output logic [3:0] led // 4-bit counter output
 );
 
-    always @(posedge clkpulse or posedge rst) begin
+    always_ff @(posedge clkpulse or posedge rst) begin
         if (rst) begin
             led <= 4'b0000; // Reset counter to 0
         end else begin
-            led <= led + 1; // Increment counter
+            if (sw0) begin
+                led <= led - 1; // Decreament counter
+            end else begin
+                led <= led + 1; // Increment counter
+            end
         end
     end
 
